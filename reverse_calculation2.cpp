d183c5957766107116a1e9c3b2c43976a4fddbc3
RobinLiang420 <liangyb0420@163.com>
2017-01-15 08:52:51 -0600

    Update reverse_calculation2.cpp

--- reverse_calculation2.cpp
+++ reverse_calculation2.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
     
     int c;
-    float x=1000;
+    float x=1000; // The reminder of Month 48th is 1000
     for(c=47;c>0;c--)
     {
         
-        x=1000+(x/(1+0.0171/12));
+        x=1000+(x/(1+0.0171/12)); // reverse calculate deposit x
         
     }
     
