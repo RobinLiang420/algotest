f71a3d3f92043c979696ac1b5150dd67e8073372
RobinLiang420 <liangyb0420@163.com>
2017-01-15 09:02:41 -0600

    Update reverse_calculation2.cpp

--- reverse_calculation2.cpp
+++ reverse_calculation2.cpp
@@ -5,12 +5,13 @@ int main() {
     
     int c;
     float x=1000; // The reminder of Month 48th is 1000
+	cout<<"in 48th month the deposit should be left 1000"<<endl;
     for(c=47;c>0;c--)
     {
         
         x=(1000+x)/(1+0.0171/12); // reverse calculate deposit x
-	cout<<"in 48th month the deposit should be left 1000";    
-        cout<<"in "<<c<<"th month the deposit should be left "<<x<<endl;
+	    
+        cout<<"in "<<c<<" month the deposit should be left "<<x<<endl;
     }
      
 	return 0;
